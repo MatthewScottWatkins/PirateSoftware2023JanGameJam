a204470dce1a11bcac6e79ca96dbbaa2646caa96
JasonAnderson201 <jasonandersons201@gmail.com>
2024-01-21 14:48:18 +1030

    Imported new player controls

--- Its Spreading/Assets/PlayerMovement.cs
+++ Its Spreading/Assets/PlayerMovement.cs
@@ -5,37 +5,53 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D rb;
+    //refs
+    private Rigidbody2D rb;
+    private Controls controls;
+    private SpriteRenderer sprite;
+    private Vector2 previousInput;
 
-    PlayerInput input;
-    PlayerActionInput inputActions;
-
-    public SpriteRenderer sprite;
-
+    [Header("Stats")]
     public float speed;
+    public float stoppingDrag;
+
+
     private void Awake()
     {
+        sprite = GetComponentInChildren<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
-        input = GetComponent<PlayerInput>();
+        controls = new Controls();
 
-        inputActions = new PlayerActionInput();
-        inputActions.Player.Enable();
+        controls.Player.Movement.performed += SetPreviousInput;
+        controls.Player.Movement.canceled += SetPreviousInput;
+        controls.Enable();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = inputActions.Player.Move.ReadValue<Vector2>();
-        float speed = 5f;
-        rb.AddForce(new Vector2(inputVector.x, inputVector.y) * speed, ForceMode2D.Force);
-
-        if (inputVector.x == -1)
+        //if trying to move
+        if (previousInput != Vector2.zero)
         {
-            sprite.flipX = true;
+            rb.AddForce(new Vector2(previousInput.x, previousInput.y) * speed, ForceMode2D.Force);
+
+            //flipping sprite based on input
+            if (previousInput.x < 0)
+                sprite.flipX = true;
+            else if (previousInput.x > 0)
+                sprite.flipX = false;
+
+            rb.drag = 1f;
         }
-        else if (inputVector.x == 1)
-        { 
-            sprite.flipX = false; 
+        else
+        {
+            rb.drag = stoppingDrag;
         }
+
+
+    }
+
+    private void SetPreviousInput(InputAction.CallbackContext ctx)
+    {
+        previousInput = ctx.ReadValue<Vector2>();
     }
 }
