8b42caa4f95f90ed57e024b2d09a555755881eea
DESKTOP-PFD60SI\mattw <s200552@students.aie.edu.au>
2024-01-27 09:16:54 +1030

    finished the main menu

---
+++ Its Spreading/Assets/HoverTween.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+
+public class HoverTween : MonoBehaviour, IPointerEnterHandler, ISelectHandler, IPointerExitHandler
+{
+    //public SoundPlayer confirmSFX = null;
+   // public SoundPlayer hoverSFX = null;
+    public float timeToTween;
+    public float tweenScale;
+
+    public bool confirm = false;
+
+    public void TweenScale(float variable)
+    {
+        transform.DOScale(variable, timeToTween);
+    }
+
+    public void TweenSelect()
+    {
+        transform.DOPunchScale(new Vector3(0.5f, 0.5f, 0.5f), timeToTween).OnComplete(() => { transform.DOScale(1, 0); });
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //hoverSFX.Play();
+        transform.DOKill();
+        TweenScale(tweenScale);    
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+
+        //confirmSFX.Play();
+        transform.DOKill();
+        TweenSelect();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOKill();
+        TweenScale(1);
+    }
+}
---
+++ Its Spreading/Assets/StartGame.cs
@@ -0,0 +1,13 @@
+using Newtonsoft.Json.Bson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void StartGameButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
